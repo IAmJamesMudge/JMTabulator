6b1e7b62987a3d7eddaf334392d564fa0fa7d565
iamjamesmudge <jamesdonaldmudge@gmail.com>
2024-11-18 15:12:36 -0800

    minimum example

--- src/components/DataTable/helpers/removeAllRanges.ts
+++
@@ -1,7 +0,0 @@
-import { TabulatorFull } from "tabulator-tables";
-
-export const removeAllRanges = (table: TabulatorFull) => {
-    table.getRanges().forEach((range) => {
-        range.remove();
-    });
-};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,1 @@
 export { default as Button } from "./Button";
-export { default as DataTable } from "./DataTable";
-export { default as FiltersTable } from "./FilterTable";
-export { default as OrderedTable } from "./OrderedTable";
